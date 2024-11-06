bfafc02fc1c33644861de15c29f5be2e731f2d1f
gr422_grmde <majagroseva6@gmail.com>
2024-11-06 12:36:36 +0700

    adminka 1

--- catalog/models.py
+++ catalog/models.py
@@ -28,26 +28,29 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
 
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return self.title
+
+def __str__(self):
+    """
+    String for representing the Model object.
+    """
+    return self.title
 
 
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular book instance.
-        """
-        return reverse('book-detail', args=[str(self.id)])
+def get_absolute_url(self):
+    """
+    Returns the url to access a particular book instance.
+    """
+    return reverse('book-detail', args=[str(self.id)])
 
-import uuid # Required for unique book instances
+import uuid  # Required for unique book instances
+
 
 class BookInstance(models.Model):
     """
     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -64,12 +67,12 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ["due_back"]
 
-
     def __str__(self):
         """
         String for representing the Model object
         """
-        return '%s (%s)' % (self.id,self.book.title)
+        return '%s (%s)' % (self.id, self.book.title)
+
 
 class Author(models.Model):
     """
@@ -85,7 +88,6 @@ class Author(models.Model):
         Returns the url to access a particular author instance.
         """
         return reverse('author-detail', args=[str(self.id)])
-
 
     def __str__(self):
         """
