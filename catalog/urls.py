61ca9af3daa1f67632b30aed4d6ecc6dd67e9ea1
gr422_grmde <majagroseva6@gmail.com>
2024-11-08 13:38:54 +0700

    page books

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
