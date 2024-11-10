a2271f2ec22fcaa831037faf8250c2ea1b0f3d5a
PC <majagroseva6@gmail.com>
2024-11-10 19:09:55 +0700

    booklist

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path
 from django.contrib import admin
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
 from django.urls import path
+
 urlpatterns = [
-     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+# Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
