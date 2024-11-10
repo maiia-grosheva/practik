cfafecbaf91ecf2da604555db1beba7fe252cde7
PC <majagroseva6@gmail.com>
2024-11-10 20:03:14 +0700

    session

--- catalog/views.py
+++ catalog/views.py
@@ -10,12 +10,14 @@ def index(request):
     num_instances=BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_authors=Author.objects.count() # Метод 'all()' применён по умолчанию.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
-        request,'index.html', context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        request,'index.html', context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_visits':num_visits},
     )
 
 from django.views import generic
